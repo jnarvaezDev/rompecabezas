e0de3566a977d96f14d864c130132b98ceeb2143
Jose Narvaez <jnarvaez@asylummarketing.com>
2025-02-19 15:57:00 -0500

    table

--- index.php
+++ index.php
@@ -54,15 +54,62 @@
                                         </tr>
                                     </thead>
                                     <tbody>
-                                    <?php
-                                        $points = 13;
-                                        for ($i = 13; $i <= 26; $i++) {
-                                            if ($points < 1) {
-                                                $points = 1;
-                                            }
-                                            echo '<tr><td>' . $i . '</td><td>' . $points-- . '</td></tr>';
-                                        }
-                                        ?>
+                                        <tr>
+                                            <td>13</td>
+                                            <td>13</td>
+                                        </tr>
+                                        <tr>
+                                            <td>14</td>
+                                            <td>12</td>
+                                        </tr>
+                                        <tr>
+                                            <td>15</td>
+                                            <td>11</td>
+                                        </tr>
+                                        <tr>
+                                            <td>16</td>
+                                            <td>10</td>
+                                        </tr>
+                                        <tr>
+                                            <td>17</td>
+                                            <td>9</td>
+                                        </tr>
+                                        <tr>
+                                            <td>18</td>
+                                            <td>8</td>
+                                        </tr>
+                                        <tr>
+                                            <td>19</td>
+                                            <td>7</td>
+                                        </tr>
+                                        <tr>
+                                            <td>20</td>
+                                            <td>6</td>
+                                        </tr>
+                                        <tr>
+                                            <td>21</td>
+                                            <td>5</td>
+                                        </tr>
+                                        <tr>
+                                            <td>22</td>
+                                            <td>4</td>
+                                        </tr>
+                                        <tr>
+                                            <td>23</td>
+                                            <td>3</td>
+                                        </tr>
+                                        <tr>
+                                            <td>24</td>
+                                            <td>2</td>
+                                        </tr>
+                                        <tr>
+                                            <td>25</td>
+                                            <td>1</td>
+                                        </tr>
+                                        <tr>
+                                            <td>26</td>
+                                            <td>1</td>
+                                        </tr>
                                     </tbody>
                                 </table>
                             </div>
